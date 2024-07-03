f8aa44e11e413b548075ae8ee4eff912305d0722
Hala Mansour <halamansour888222555@gmail.com>
2024-07-03 13:33:55 +0300

    initial commit

---
+++ Polling Page Administration System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Polling_Page_Administration_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
